7c78e7e6743e4e49cccafd92c823dc6029de29d9
luiizsilverio <luiiz.silverio@gmail.com>
2022-02-26 15:29:00 -0300

    rotas do CRUD

--- app/Controllers/Http/MomentsController.ts
+++ app/Controllers/Http/MomentsController.ts
@@ -1,12 +1,33 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Application from '@ioc:Adonis/Core/Application'
+import { v4 as uuid } from 'uuid'
 
 import Moment from 'App/Models/Moment'
 
 export default class MomentsController {
+    private validationOptions = {
+        types: ['image'],
+        size: '2mb'
+    }
 
+    // POST: /api/moments
     // inserção de dados (store ou outro nome)
     public async store({ request, response }: HttpContextContract) {
+        // o body não é JSON, é Multipart-data
         const body = request.body()
+
+        const image = request.file('image', this.validationOptions)
+
+        // upload de imagens
+        if (image) {
+            const imageName = `${uuid()}.${image.extname}`
+
+            await image.move(Application.tmpPath('uploads'), {
+                name: imageName
+            })
+
+            body.image = imageName
+        }
 
         // insere os dados no banco
         const moment = await Moment.create(body)
@@ -18,4 +39,33 @@ export default class MomentsController {
         }
     }
 
+    // GET: /api/moments
+    public async index() {
+      const moments = await Moment.all()
+
+      return {
+        data: moments
+      }
+    }
+
+    // GET: /api/moments/:id
+    public async show({params}: HttpContextContract) {
+      const moment = await Moment.findOrFail(params.id)
+
+      return {
+        data: moment
+      }
+    }
+
+    // DELETE: /api/moments/:id
+    public async destroy({params}: HttpContextContract) {
+      const moment = await Moment.findOrFail(params.id)
+
+      await moment.delete()
+
+      return {
+        message: "Excluído com sucesso",
+        data: moment
+      }
+    }
 }
